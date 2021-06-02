e88fa40b485dc22869ab1882718e75d5ebd97aa4
Magdaléna Pazlarová <m.pazlarova@gmail.com>
2021-06-02 12:05:42 +0200

    vytvoření react projektu

---
+++ src/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+import './style.css';
+
+const App = () => {
+  return <h1>KontraKaos</h1>;
+};
+render(<App />, document.querySelector('#app'));
