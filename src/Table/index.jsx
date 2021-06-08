b41a3576e963de8189000a206794014505a63316
Magdaléna Pazlarová <m.pazlarova@gmail.com>
2021-06-08 11:56:57 +0200

    úprava tabulky + rozdělení do dnů práce/den

--- src/Table/index.jsx
+++ src/Table/index.jsx
@@ -2,59 +2,46 @@ import React from 'react';
 import './style.css';
 
 export const Table = ({ data }) => {
-  console.log('jjj', data);
-
-  /*
-  (2) [{…}, {…}]
-  0: {name: "Joan", days: Array(7)}
-  1: {name: "Mona", days: Array(7)}
-*/
-
-  /*
-  0:
-  days: Array(7)
-  0: {tasks: Array(0)}
-  1: {tasks: Array(0)}
-  2: {tasks: Array(0)}
-  3: {tasks: Array(0)}
-  4: {tasks: Array(0)}
-  5: {tasks: Array(0)}
-  6: {tasks: Array(0)}
-  length: 7
-
-name: "Joan"*/
   return (
     <>
       <h2>Rozvrh domácích prací</h2>
       <table>
-        <tr className="dny">
-          <th>Osoba</th>
-          <th>Pondělí</th>
-          <th>Útery</th>
-          <th>Středa</th>
-          <th>Čtvrtek</th>
-          <th>Pátek</th>
-          <th>Sobota</th>
-          <th>Neděle</th>
-        </tr>
-
+        <thead>
+          <tr className="dny">
+            <th>Osoba</th>
+            <th>Pondělí</th>
+            <th>Útery</th>
+            <th>Středa</th>
+            <th>Čtvrtek</th>
+            <th>Pátek</th>
+            <th>Sobota</th>
+            <th>Neděle</th>
+          </tr>
+        </thead>
         {data.map((dt) => {
-          console.log(dt.name);
-          console.log(dt.days);
           return (
-            <tr>
-              <td>{dt.name}</td>
+            <tbody>
+              <tr>
+                <td>{dt.name}</td>
 
-              {dt.days.map((day) => {
-                return (
-                  <td>
-                    {day.tasks.map((task) => {
-                      return <p>{task.jmeno}</p>;
-                    })}
-                  </td>
-                );
-              })}
-            </tr>
+                {dt.days.map((day) => {
+                  return (
+                    <>
+                      {day.tasks.map((task) => {
+                        return <td>{task.jmeno}</td>;
+                      })}
+                    </>
+                    /*
+                      <td>
+                        {day.tasks.map((task) => {
+                          return <p>{task.jmeno}</p>;
+                        })}
+                      </td>
+                    */
+                  );
+                })}
+              </tr>
+            </tbody>
           );
         })}
       </table>
