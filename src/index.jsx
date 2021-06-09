2163c11c59744edaa2b46f201e356ba081aabb05
Veronika Prokůpková <prokupkova.verca@gmail.com>
2021-06-09 07:55:23 +0200

    homepageStyles

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { render } from 'react-dom';
 import './style.css';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
@@ -8,81 +8,96 @@ import { About } from './About';
 import { Blog } from './Blog';
 
 const App = () => {
+  const [textContent, setTextContent] = useState('');
+
   return (
     <>
       <Router>
-        <div className="page">
-          <nav>
-            <div className="nav__logo">
-              <a href="index.html">
-                <img
-                  className="logo__img"
-                  src="./assets/logo_prototyp.png"
-                  alt=""
-                />
-              </a>
-              <h2>KontraKaos</h2>
-            </div>
-
+        <nav>
+          <div className="logo">
+            <Link
+              to="/"
+              className="nav__home"
+              onClick={() => setTextContent('')}
+            >
+              {' '}
+              <img
+                className="logo__img"
+                src="./assets/logo_prototyp_no_background.svg"
+                alt=""
+                onClick={() => setTextContent('')}
+              />
+            </Link>
+            <h1 className="h1__hidden">{textContent}</h1>
+          </div>
+          <div className="nav__bar">
             <div className="nav__links">
-              <Link to="/" className="nav__home">
-                Home
-              </Link>
-
-              <Link to="/about" className="nav__about">
+              <Link
+                to="/about"
+                className="nav__about"
+                onClick={() => setTextContent('KontraKaos')}
+              >
                 O nás
               </Link>
 
-              <Link to="/kontrakaos" className="nav__app">
+              <Link
+                to="/kontrakaos"
+                className="nav__app"
+                onClick={() => setTextContent('KontraKaos')}
+              >
                 Aplikace
               </Link>
 
-              <Link to="/blog" className="nav__blog">
+              <Link
+                to="/blog"
+                className="nav__blog"
+                onClick={() => setTextContent('KontraKaos')}
+              >
                 Blog
               </Link>
             </div>
-          </nav>
-          <Switch>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/kontrakaos">
-              <Aplikace />
-            </Route>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/blog">
-              <Blog />
-            </Route>
-          </Switch>
+          </div>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/kontrakaos">
+            <Aplikace />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/blog">
+            <Blog />
+          </Route>
+        </Switch>
 
-          <footer>
-            <div>Vytvořeno v rámci Czechitas DA©</div>
-            <div>
-              <a target="_blank" href="mailto:m.pazlarova@gmail.com">
-                Napište nám
-              </a>
-            </div>
-            <div className="icons__social">
-              <a
-                target="_blank"
-                href="https://www.facebook.com/magda.pazlarova/"
-              >
-                <img height={20} src="./assets/fb.svg" alt="Facebook" />
-              </a>
-              <a target="_blank" href="https://www.instagram.com/m.pazlar/">
-                <img height={20} src="./assets/ig.svg" alt="Instagram" />
-              </a>
-              <a
-                target="_blank"
-                href="https://www.youtube.com/channel/UCtK3lKupYOpFya4_QskBQFg"
-              >
-                <img height={20} src="./assets/yt.svg" alt="Youtube" />
-              </a>
-            </div>
-          </footer>
-        </div>
+        <footer>
+          <div>Vytvořeno v rámci Czechitas DA©</div>
+          <div>
+            <a target="_blank" href="mailto:m.pazlarova@gmail.com">
+              Napište nám
+            </a>
+          </div>
+          <div className="icons__social">
+            <a target="_blank" href="https://www.facebook.com/KontraKaos">
+              <img height={20} src="./assets/fb.svg" alt="Facebook" />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/m.pazlar/">
+              <img height={20} src="./assets/ig.svg" alt="Instagram" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.youtube.com/channel/UCtK3lKupYOpFya4_QskBQFg"
+            >
+              <img height={20} src="./assets/yt.svg" alt="Youtube" />
+            </a>
+            <a target="_blank" href="https://github.com/mpazlar/kontrakaos.git">
+              <img height={20} src="./assets/gh.svg" alt="GitHub" />
+            </a>
+          </div>
+        </footer>
       </Router>
     </>
   );
